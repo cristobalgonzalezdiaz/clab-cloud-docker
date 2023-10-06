b6b9fd78e06564b1ffcfcac9b58e8659c94ffc88
Cristobal Gonzalez <cristobal@cristobal-lab.net>
2023-10-06 15:06:01 +1100

    Python images

---
+++ python/Base-Image/d_build_python_base.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# This script is used to build the docker image for the project
+
+docker build -t clab-python:1.0.0 .
---
+++ python/FastAPI/start_app.py
@@ -0,0 +1,10 @@
+# your_app.py
+
+# This is a placeholder Python script.
+# You can use this file to run your code manually within the Docker container.
+
+def main():
+    print("Welcome to your Python application!")
+
+if __name__ == "__main__":
+    main()
